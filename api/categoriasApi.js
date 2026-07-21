2c37c7aa0b32a666167edea5637be035ff6d1336
cecilia <skadi.cr24@gmail.com>
2026-07-21 17:57:29 -0300

    borrando todo y empezando de nuevo

--- api/categoriasApi.js
+++ api/categoriasApi.js
@@ -1,12 +1,1 @@
 
-export async function obtenerCategorias() {
-
-    const respuesta = await fetch(
-        "https://jsonplaceholder.typicode.com/users"
-    );
-
-    const datos = await respuesta.json();
-
-    return datos.map(usuario => usuario.name);
-
-}
--- main.js
+++ main.js
@@ -1,15 +1,1 @@
 
-import { obtenerCategorias } from "./api/categoriasApi.js";
-import { ControladorTareas } from "./controladores/ControladorTareas.js";
-import "./styles/styles.css";
-
-obtenerCategorias()
-.then(categorias => {
-
-    console.log(categorias);
-
-});
-
-const app = new ControladorTareas();
-
-app.iniciar();
